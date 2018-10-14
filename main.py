0bc2fc67fbe9dcec52f4dbdfd33c016410d312f3
Yichen <ichisaur@gmail.com>
2018-10-14 00:58:54 -0500

    Add file import

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import numpy as np 
 import imageio
 from PIL import Image
+
+# Names of the .bmp sample files
+fileNames = [   'gun.bmp', 
+                'palm.bmp']
+
+#bmp import dict
+bmp = {}
+
+#Import the bmps
+for loc in fileNames:
+    bmp[loc] = np.array(imageio.imread(loc))
